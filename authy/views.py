96bf1a2472b150ccc51ffa77e88f7ff0719eb1c0
Miandry05 <86937367+MiandryRakoto@users.noreply.github.com>
2021-10-15 08:20:28 +0300

    taskify

--- authy/views.py
+++ authy/views.py
@@ -1,8 +1,12 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
 from django.urls.base import reverse_lazy
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 class CustomLoginView(LoginView):
     template_name = 'authy/login.html'
@@ -11,3 +15,22 @@ class CustomLoginView(LoginView):
     
     def get_success_url(self):
         return reverse_lazy('tasks') 
+    
+class RegisterPage(FormView):
+    template_name = 'authy/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('login')
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    # def get(self, *args, **kwargs):
+    #     if self.request.user.is_authenticated:
+    #         return redirect('login')
+    #     return super(RegisterPage, self).get(*args, **kwargs)
+    
+        
